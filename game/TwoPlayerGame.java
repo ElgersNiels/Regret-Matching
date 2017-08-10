4840e9430fa5f3fef5ef73356158fe92dc1d89c8
Unknown <elgersniels@gmail.com>
2017-08-11 00:52:52 +0200

    Added constructor taking Matrix<Double>.

--- game/TwoPlayerGame.java
+++ game/TwoPlayerGame.java
@@ -1,12 +1,16 @@
 package game;
 public class TwoPlayerGame extends Game {
 
-	@SuppressWarnings("unchecked")
 	public TwoPlayerGame(double[][] p1, double[][] p2) {
-		super((Matrix<Double>[]) new Matrix[]{buildMatrix(p1), buildMatrix(p2)});
+		this(buildMatrix(p1), buildMatrix(p2));
 	}
 	
-	private static Matrix<Double> buildMatrix(double[][] payoffs) {
+	@SuppressWarnings("unchecked")
+	public TwoPlayerGame(Matrix<Double> p1, Matrix<Double> p2) {
+		super((Matrix<Double>[]) new Matrix[]{p1, p2});
+	}
+	
+	protected static Matrix<Double> buildMatrix(double[][] payoffs) {
 		Matrix<Double> matrix = new Matrix<Double>(new int[]{payoffs.length, payoffs[0].length});
 		
 		for (int i = 0; i < payoffs.length; i++)
