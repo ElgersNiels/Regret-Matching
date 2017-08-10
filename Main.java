71e2df3fdc020327cd03c31020986efb97f77742
Unknown <elgersniels@gmail.com>
2017-08-11 00:28:57 +0200

    irrelevant changes

--- Main.java
+++ Main.java
@@ -1,40 +1,43 @@
 import java.util.Arrays;
 
 import game.Game;
+import game.TwoPlayerGame;
+import game.TwoPlayerZeroSumGame;
 import parser.GameParser;
 import regret.RegretMatching;
-import regret.TwoPlayerRegretMatching;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Some game (zero-sum)");
-		TwoPlayerRegretMatching r = new TwoPlayerRegretMatching(
+		//--
+		Game g = new TwoPlayerGame(
 				new double[][]{{1,3,-1},{2,-2,3}}, 
-				new double[][]{{-1,-3,1},{-2,2,-3}});
+				new double[][]{{-1,-3,1},{-2,2,-3}}
+				);
+		RegretMatching r = new RegretMatching(g);
 		
-		r.train((int) 1e7);
+		r.train((int) 1e6);
 		
 		for (double[] s : r.getTrainedStrategy())
 			System.out.println(Arrays.toString(s));
-//		
-//		System.out.println("RPS (zero-sum)");
-//		r = new TwoPlayerRegretMatching(
-//				new double[][]{{0,-1,1},{1,0,-1},{-1,1,0}}, 
-//				new double[][]{{0,1,-1},{-1,0,1},{1,-1,0}});
-//		
-//		r.train((int) 10e2);
-//		
-//		for (double[] s : r.getTrainedStrategy())
-//			System.out.println(Arrays.toString(s));
 		
+		//--
+		g = new TwoPlayerZeroSumGame(new double[][]{{1,3,-1},{2,-2,3}});
+		r = new RegretMatching(g);
+		
+		r.train((int) 1e6);
+		
+		for (double[] s : r.getTrainedStrategy())
+			System.out.println(Arrays.toString(s));
+		
+		//--
 		GameParser gp = new GameParser();
-		Game game = gp.parse("C:/Users/elger/Desktop/game.txt");
+		g = gp.parse("C:/Users/elger/Desktop/game.txt");
 		
-		RegretMatching t = new RegretMatching(game);
-		t.train((int) 1e7);
+		r = new RegretMatching(g);
+		r.train((int) 1e6);
 		
-		for (double[] s : t.getTrainedStrategy())
+		for (double[] s : r.getTrainedStrategy())
 			System.out.println(Arrays.toString(s));
 	}
 
